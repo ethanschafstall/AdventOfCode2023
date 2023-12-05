4157c681c0cf1917d2a54dc7f87df819e9959ac2
ethanschafstall <ethans9999@gmail.com>
2023-12-05 11:12:18 +0100

    Created tool for TextToString

---
+++ code/AdventOfCode2023/Tools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Tools
+    {
+        public static string[] TextToString(string path) 
+        {
+            
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            return lines;
+        }
+    }
+}
