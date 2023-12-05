11fc04e07e363c031d93bc4786c27111ee2de18a
ethanschafstall <ethans9999@gmail.com>
2023-12-05 11:23:16 +0100

    Updated program.cs

--- code/AdventOfCode2023/Program.cs
+++ code/AdventOfCode2023/Program.cs
@@ -21,12 +21,25 @@ namespace AdventOfCode
                         Console.WriteLine($"Day One Answer: {Day1(Tools.TextToString(@"C:\Users\ethschafsta\Documents\GitHub\AdventOfCode2023\code\textFiles\day1puzzleInput.txt"))}");
                         break;
                     case 2:
-                        Console.WriteLine($"Day One Answer: {Day2(Tools.TextToString(@"C:\Users\ethschafsta\Documents\GitHub\AdventOfCode2023\code\textFiles\day1puzzleInput.txt"))}");
+                        Console.WriteLine($"Day One Answer: {Day2(Tools.TextToString(@"C:\Users\ethschafsta\Documents\GitHub\AdventOfCode2023\code\textFiles\day2puzzleInput.txt"))}");
                         break;
                 }
             }
             Console.ReadLine();
 
+        }
+
+        enum numbers 
+        { 
+            one = 1,
+            two = 2,
+            three = 3,
+            four = 4,
+            five = 5,
+            six = 6,
+            seven = 7,
+            eight = 8,
+            nine = 9,
         }
 
         /// <summary>
@@ -35,7 +48,7 @@ namespace AdventOfCode
         static int Day1(string[] inputs)
         {
             // Regex pattern to find all digits
-            string pattern = @"\d+";
+            string pattern = "one|two|three|four|five|six|seven|eight|nine";
 
             // string list for holding the double digit numbers in string form
             List<string> doubleDigitNum = new List<string>();
@@ -48,6 +61,10 @@ namespace AdventOfCode
             // loops through each line of input
             for (int i = 0; i < inputs.Length; i++)
             {
+                if (regex.IsMatch(inputs[i]))
+                {
+
+                }
                 // loops through each char from the input string
                 foreach (var item in inputs[i])
                 {
